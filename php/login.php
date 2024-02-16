bf05c84b237a3375b7b305d8c0b30a9e56cb161d
JLHuerta13 <jorgeluishuerta3@gmail.com>
2024-02-15 22:13:56 -0600

    cambios 15/02/2024

--- php/login.php
+++ php/login.php
@@ -1,4 +1,20 @@
 <?php
+// Datos de conexión a la base de datos (reemplaza con tus propios datos)
+$servername = "localhost"; // Nombre del servidor
+$username = "root"; // Nombre de usuario de la base de datos
+$password = ""; // Contraseña de la base de datosC
+$dbname = "usuarios_login"; // Nombre de la base de datos
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Error en la conexión a la base de datos: " . $conn->connect_error);
+}
+
+$sql = "SELECT correo, palabra_secreta FROM usuarios";
+
 # Nota: no estamos haciendo validaciones
 $correo = $_POST["correo"];
 $palabra_secreta = $_POST["palabra_secreta"];
